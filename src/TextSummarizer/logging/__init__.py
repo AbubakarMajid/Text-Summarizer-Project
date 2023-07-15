82b476a770ba2f731914e6148393a38c6ecc8c24
ABUZAR MAJID <abubakarmajid036@gmail.com>
2023-07-16 03:33:14 +0500

    logger

---
+++ src/TextSummarizer/logging/__init__.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import logging
+
+loging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filename = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = loging_str,
+    
+    handlers = [
+        logging.FileHandler(log_filename),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("TestSummarizerLogger")
