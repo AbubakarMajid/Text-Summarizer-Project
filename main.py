0c2eaf5e464fbab6c79841f416f6b5a3a8fee5b3
ABUZAR MAJID <abubakarmajid036@gmail.com>
2023-08-01 23:12:27 +0500

    Data Validation done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from TextSummarizer.pipeline.stage01 import dataIngestionTrainingPipeline
+from TextSummarizer.pipeline.stage02 import dataValidationTrainingPipeline
 from TextSummarizer.logging import logger
 
-STAGE_NAME = "data Ingestion stage"
+STAGE_NAME = "data ingestion stage"
 
 try:
-    logger.info(">>>>> Stage {STAGE_NAME} started >>>>>>")
+    logger.info(">>>>> Stage {ST\AGE_NAME} started >>>>>>")
     data_ingestion = dataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info("<<<<<<<< stage {STAGE_NAME} completed >>>>>>>")
@@ -12,3 +13,17 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "data validation stage"
+
+try:
+    logger.info(">>>>> Stage {STAGE_NAME} started >>>>>>")
+    data_validation = dataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info("<<<<<<<< stage {STAGE_NAME} completed >>>>>>>")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
--- src/TextSummarizer/entity/__init__.py
+++ src/TextSummarizer/entity/__init__.py
@@ -8,3 +8,10 @@ class DataIngestionConfig:
    local_data_files: Path
    unzip_dir: Path
     
+
+@dataclass(frozen = True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE:str
+    ALL_REQUIRED_FILES: list
+    
